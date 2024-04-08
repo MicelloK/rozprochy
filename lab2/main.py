1a383b76086d3dbbb10c1ef77287ade9a5bb8de2
Michał Kobiera <kobiera@zoho.com>
2024-04-08 14:27:26 +0200

    Update api error handling

--- lab2/main.py
+++ lab2/main.py
@@ -21,7 +21,7 @@ def error_page(status_code: int):
      return HTMLResponse(f"""
                          <div style="text-align: center;">
                               <h1>Brak PIWA mój Panie! :(</h1><img src="https://http.cat/{status_code}">
-                         </div>""")
+                         </div>""", status_code)
 
 @app.get('/search')
 async def search(city_name: str, address: str, api_key: str):
@@ -31,23 +31,29 @@ async def search(city_name: str, address: str, api_key: str):
      # getting brewery list by city
      url = f"https://api.openbrewerydb.org/v1/breweries?by_city={city_name}"
      resp = requests.get(url)
+     
+     if not resp.ok:
+          return error_page(resp.status_code)
+     
      brewery_list = resp.json()
      
      if not brewery_list:
           return error_page(400)
-     if not resp.ok:
-          return error_page(resp.status_code)
+     
      
      # getting longitude and latitude by address
      address_api_key = "177cb2528f0c4a8392cc36ecb9f1925b"
      url = f"https://api.geoapify.com/v1/geocode/search?text={address}&apiKey={address_api_key}"
      resp = requests.get(url)
+     
+     if not resp.ok:
+          return error_page(resp.status_code)
+     
      geocoding = resp.json()
      
      if not geocoding['features']:
           return error_page(400)
-     if not resp.ok:
-          return error_page(resp.status_code)
+     
      
      current_lon = geocoding['features'][0]['properties']['lon']
      current_lat = geocoding['features'][0]['properties']['lat']
@@ -66,12 +72,13 @@ async def search(city_name: str, address: str, api_key: str):
           # getting brewery elevation
           url = f'https://api.open-elevation.com/api/v1/lookup?locations={lat},{lon}'
           resp = requests.get(url)
-          elevation = resp.json()
-          elevation = elevation['results'][0]['elevation']
-          brewery_elevations.append((elevation, brewery))
           
           if not resp.ok:
                return error_page(resp.status_code)
+          
+          elevation = resp.json()
+          elevation = elevation['results'][0]['elevation']
+          brewery_elevations.append((elevation, brewery))
           
           postal_code_mean[0] += int(brewery['postal_code'][:2])
           postal_code_mean[1] += int(brewery['postal_code'][3:])
